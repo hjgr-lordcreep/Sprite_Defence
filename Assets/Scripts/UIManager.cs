52bc4d57fd663841c8bd18fff9f0f00338bb3e46
lordcreep <hjgr.lordcreep@gmail.com>
2024-12-05 11:48:52 +0900

    UI 시작 끝 임시구현

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,27 +1,78 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject startGameUI;
+    public GameObject inGameUI;
+    public GameObject endGameUI;
+
+
     public TextMeshProUGUI timeText;
     private float surviveTime;
-    private bool isDead;
+    private bool isLive;
 
-    private void Start()
+    private void Awake()
+    {
+        isLive = false;
+
+    }
+
+
+
+    public void GameStart()
     {
         surviveTime = 0;
-        isDead = false;
+        Time.timeScale = 1;
+        inGameUI.gameObject.SetActive(true);
+        isLive = true;
 
-        
     }
+
+    public void GameOver()
+    {
+        StartCoroutine(GameOverRoutine());
+
+    }
+
+    IEnumerator GameOverRoutine()
+    {
+        isLive = false;
+
+        yield return new WaitForSeconds(0.5f);
+
+        inGameUI.gameObject.SetActive(false);
+        endGameUI.gameObject.SetActive(true);
+        Time.timeScale = 0;
+
+
+    }
+
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+
     private void Update()
     {
-        if (!isDead)
+        if (!isLive) return;
+        
+        surviveTime += Time.deltaTime;
+        timeText.text = "Time : " + surviveTime.ToString("F1");
+        
+
+        if (surviveTime > 5)
         {
-            surviveTime += Time.deltaTime;
-            timeText.text = "Time : " + surviveTime.ToString("F1");
+            isLive = false;
+            GameOver();
         }
+        
+        
 
         //µð¹ö±ë¿ë
         if (Input.GetKeyDown(KeyCode.F10))
