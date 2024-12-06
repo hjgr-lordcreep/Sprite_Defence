690b1697365ef5cc73e3771400c54ce953c94d7c
DESKTOP-IFUE7J8\admin <kns7208@naver.com>
2024-12-06 14:50:33 +0900

    포트리스 체력 변경

--- Assets/Scripts/Zombie/LivingEntity.cs
+++ Assets/Scripts/Zombie/LivingEntity.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 // 생명체로서 동작할 게임 오브젝트들을 위한 뼈대를 제공
 // 체력, 데미지 받아들이기, 사망 기능, 사망 이벤트를 제공
 public class LivingEntity : MonoBehaviour, IDamageable {
-    public float startingHealth = 100f; // 시작 체력
-    public float health { get; protected set; } // 현재 체력
+    public virtual float startingHealth = 100f; // 시작 체력
+    public virtual float health { get; protected set; } // 현재 체력
     public bool IsDead { get { return !gameObject.activeSelf; } set { gameObject.SetActive(value); } } // 사망 상태
     //public event Action onDeath; // 사망시 발동할 이벤트
 
